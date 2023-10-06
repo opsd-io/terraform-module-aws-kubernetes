7cb9d91e10498c52971e0b7fa72e2415de6e4c80
Piotr Stawarski <piotr.stawarski@zdt.io>
2023-10-06 15:25:35 +0200

    code

--- main.tf
+++ main.tf
@@ -1,1 +1,64 @@
-# Terraform code goes here
+terraform {
+  required_version = ">= 1.3.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+resource "aws_eks_cluster" "main" {
+  name     = var.name
+  version  = var.k8s_version
+  role_arn = aws_iam_role.cluster.arn
+  tags = merge(var.common_tags, {
+    Name = var.name
+  })
+
+  enabled_cluster_log_types = var.enabled_cluster_log_types
+
+  vpc_config {
+    subnet_ids              = var.subnet_ids
+    endpoint_private_access = var.endpoint_private_access
+    endpoint_public_access  = var.endpoint_public_access
+    public_access_cidrs     = var.public_access_cidrs
+    security_group_ids      = var.security_group_ids
+  }
+
+  kubernetes_network_config {
+    ip_family         = "ipv4"
+    service_ipv4_cidr = "172.20.0.0/16"
+  }
+
+  #   encryption_config {
+  #     provider {
+  #       key_arn = "aaa"
+  #     }
+  #     resources = ["secrets"]
+  #   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKSFargatePodExecutionRolePolicy,
+    aws_cloudwatch_log_group.cluster,
+  ]
+}
+
+resource "aws_cloudwatch_log_group" "cluster" {
+  name              = "/aws/eks/${var.name}/cluster"
+  retention_in_days = var.cluster_log_retention_in_days
+}
+
+data "tls_certificate" "oidc_issuer" {
+  url = aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks_cluster" {
+  url = data.tls_certificate.oidc_issuer.url
+  thumbprint_list = [
+    data.tls_certificate.oidc_issuer.certificates[0].sha1_fingerprint
+  ]
+  client_id_list = ["sts.amazonaws.com"]
+}
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,59 @@
-# output "variable" {
-#   description = "output variable description"
-#   value       = variable.main.name
-# }
+output "id" {
+  value = aws_eks_cluster.main.id
+}
+
+output "arn" {
+  value = aws_eks_cluster.main.arn
+}
+
+output "name" {
+  value = aws_eks_cluster.main.name
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.main.endpoint
+}
+
+output "version" {
+  value = aws_eks_cluster.main.version
+}
+
+output "cluster_ca" {
+  value = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+}
+
+output "oidc_issuer" {
+  value = aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
+output "openid_arn" {
+  value = aws_iam_openid_connect_provider.eks_cluster.arn
+}
+
+output "openid_sub" {
+  value = format("%s:sub", trimprefix(aws_iam_openid_connect_provider.eks_cluster.url, "https://"))
+}
+
+output "cluster_role_arn" {
+  value = aws_iam_role.cluster.arn
+}
+
+output "cluster_role_name" {
+  value = aws_iam_role.cluster.name
+}
+
+output "node_role_arn" {
+  value = aws_iam_role.node.arn
+}
+
+output "node_role_name" {
+  value = aws_iam_role.node.name
+}
+
+output "fargate_role_arn" {
+  value = aws_iam_role.fargate.arn
+}
+
+output "fargate_role_name" {
+  value = aws_iam_role.fargate.name
+}
